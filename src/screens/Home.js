2d5103c62624edce18c287af917afd862f69ad3b
Arpit Gupta <arpit.8.a@gmail.com>
2022-03-13 15:12:26 +0530

    Chakra UI Setup

---
+++ src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Text, Heading, Button } from "@chakra-ui/react";
+
+function Home() {
+  return (
+    <Box d="flex" justifyContent={"center"} alignItems={"center"} w={"100%"}>
+      <Box
+        d="flex"
+        justifyContent={"center"}
+        w={["100%", null, null, "70%"]}
+        // h={["100vh", null, null, "100vh"]}
+        // backgroundColor={"#000000"}
+      ></Box>
+    </Box>
+  );
+}
+
+export default Home;
